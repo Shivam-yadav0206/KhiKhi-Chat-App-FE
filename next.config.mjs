411f1a64703e2b7dbcb602d4a60eb1e761d630bc
Shivam Yadav <sy7176442@gmail.com>
2024-04-16 12:58:12 +0530

    Trying

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    images: {
-        domains: ['lh3.googleusercontent.com'],
-    }
-
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 export default nextConfig;
